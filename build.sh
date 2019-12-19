8cef42159dc3f0fe58263a90450157a9b2f8ff14
Lev E <bearnode@gmail.com>
2019-12-19 22:03:25 +0300

    update build sh

--- build.sh
+++ build.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
 
-env GOOS=linux GOARCH=arm GOARM=6 go build -o cmd/main.go -o release/pi-temp &&
-    tar -czvf release/pi-temp.tar.gz release/pi-temp
+env GOOS=linux GOARCH=arm GOARM=6 go build -o cmd/main.go -o release/pi-temp
