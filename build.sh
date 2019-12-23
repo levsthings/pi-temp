4ea2331dcb8dc3a01c1d491f492a2a7e73b8c8d8
Lev E <bearnode@gmail.com>
2019-12-23 11:23:11 +0300

    update build script

--- build.sh
+++ build.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
 
-env GOOS=linux GOARCH=arm GOARM=6 go build -o release/pi-temp cmd/main.go
+tag="$(git describe --tags $(git rev-list --tags --max-count=1))"
+filename="pi-temp-$tag-arm64-linux"
+
+env GOOS=linux GOARCH=arm GOARM=6 go build -o release/pi-temp cmd/main.go &&
+    cd release && tar -czvf $filename.tar.gz pi-temp
